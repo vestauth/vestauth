a40e118bf55df88425e337ec658fab071936d7eb
Scott Motte <mot@mot.la>
2026-01-15 10:19:52 -0800

    Fix path

--- src/lib/helpers/hash.js
+++ src/lib/helpers/hash.js
@@ -1,5 +1,5 @@
-const { sha256 } = require('@noble/hashes/sha256')
-const { utf8ToBytes } = require('@noble/hashes/utils')
+const { sha256 } = require('@noble/hashes/sha2.js')
+const { utf8ToBytes } = require('@noble/hashes/utils.js')
 
 function hash (message = 'hello') {
   return sha256(utf8ToBytes(message))
