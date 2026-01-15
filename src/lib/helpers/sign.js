35aa3cb16ad35b8affabf5d005f653c449cd32c1
Scott Motte <mot@mot.la>
2026-01-15 10:44:50 -0800

    hash

--- src/lib/helpers/sign.js
+++ src/lib/helpers/sign.js
@@ -1,5 +1,10 @@
 // import { sign, verify, getPublicKey } from '@noble/secp256k1'
 const secp = require('@noble/secp256k1')
+const { sha256 } = require('@noble/hashes/sha2.js')
+const { hmac } = require('@noble/hashes/hmac.js')
+secp.hashes.sha256 = sha256
+secp.hashes.hmacSha256 = (key, msg) => hmac(sha256, key, msg)
+const hash = require('./helpers/hash')
 
 // // privateKey: 32-byte Uint8Array (or hex depending on your setup)
 // const signature = await sign(hash, privateKey, { der: false })
@@ -11,7 +16,7 @@ const secp = require('@noble/secp256k1')
 // const ok = verify(signature, hash, publicKey)
 
 function sign (message, privateKey) {
-  return secp.sign(message, privateKey)
+  return secp.sign(hash(message), hash(privateKey))
 }
 
 module.exports = sign
