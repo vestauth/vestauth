1bb0515d3291177019028c83327a750f830b0127
Scott Motte <mot@mot.la>
2026-01-20 13:17:34 -0800

    allow for formatted keys

--- src/lib/helpers/verify.js
+++ src/lib/helpers/verify.js
@@ -1,7 +1,9 @@
 const secp = require('@noble/secp256k1')
 const hash = require('./hash')
+const stripFormatting = require('./stripFormatting')
 
-function verify (challenge, signatureBase64, publicKeyHex) {
+function verify (challenge, signatureBase64, publicKeyHexPossiblyFormatted) {
+  const publicKeyHex = stripFormatting(publicKeyHexPossiblyFormatted)
   const hashChallenge = hash(challenge)
   const signature = Buffer.from(signatureBase64, 'base64url')
   const publicKeyBytes = Buffer.from(publicKeyHex, 'hex')
