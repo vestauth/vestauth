3f823c0baafc24d55d3dfe94841d2e7cee035603
Scott Motte <mot@mot.la>
2026-01-21 14:21:09 -0800

    verify authorization header

--- src/lib/main.js
+++ src/lib/main.js
@@ -3,11 +3,13 @@ const hash = require('./helpers/hash')
 const keypair = require('./helpers/keypair')
 const sign = require('./helpers/sign')
 const verify = require('./helpers/verify')
+const verifyAuthorizationHeader = require('./helpers/verifyAuthorizationHeader')
 
 module.exports = {
   challenge,
   hash,
   keypair,
   sign,
-  verify
+  verify,
+  verifyAuthorizationHeader
 }
