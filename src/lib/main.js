aaed4b026d2da04c903b097e26d2c4b5e7a04eab
Scott Motte <mot@mot.la>
2026-01-22 12:35:25 -0800

    move around

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,17 +1,9 @@
-const challenge = require('./helpers/challenge')
-const hash = require('./helpers/hash')
-const keypair = require('./helpers/keypair')
-const sign = require('./helpers/sign')
-const verify = require('./helpers/verify')
 const verifyAuthorizationHeader = require('./helpers/verifyAuthorizationHeader')
 const verifyAgent = require('./helpers/verifyAgent')
+const primitives = require('./primitives')
 
 module.exports = {
-  challenge,
-  hash,
-  keypair,
-  sign,
-  verify,
+  primitives,
   verifyAuthorizationHeader,
   verifyAgent
 }
---
+++ src/lib/primitives.js
@@ -0,0 +1,13 @@
+const challenge = require('./helpers/challenge')
+const hash = require('./helpers/hash')
+const keypair = require('./helpers/keypair')
+const sign = require('./helpers/sign')
+const verify = require('./helpers/verify')
+
+module.exports = {
+  challenge,
+  hash,
+  keypair,
+  sign,
+  verify
+}
