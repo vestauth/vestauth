dd9cc71da955f31e0a3044f6fe5a3b1820d68c2d
Scott Motte <mot@mot.la>
2026-01-30 15:17:37 -0800

    more clean up

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,5 +1,3 @@
-const verifyAuthorizationHeader = require('./helpers/verifyAuthorizationHeader')
-const verifyAgent = require('./helpers/verifyAgent')
 const agent = require('./agent')
 const provider = require('./provider')
 const primitives = require('./primitives')
@@ -7,7 +5,5 @@ const primitives = require('./primitives')
 module.exports = {
   agent,
   provider,
-  primitives,
-  verifyAuthorizationHeader,
-  verifyAgent
+  primitives
 }
