896238a18078d8c116614876526e1fa02c348978
Scott Motte <mot@mot.la>
2026-02-04 11:46:46 -0800

    adjust note

--- src/lib/helpers/authorityMessage.js
+++ src/lib/helpers/authorityMessage.js
@@ -2,6 +2,13 @@ function authorityMessage (uri, signatureParams) {
   const u = new URL(uri)
   const authority = u.host // includes port if present
 
+  // other possible message (target-uri, and signature-params)
+  // const message = [
+  //   `"@method": ${method.toUpperCase()}`,
+  //   `"@target-uri": ${uri}`,
+  //   `"@signature-params": ${signatureParams}`
+  // ].join('\n')
+
   return [
     `"@authority": ${authority}`,
     `"@signature-params": ${signatureParams}`
--- src/lib/helpers/webBotAuthSignature.js
+++ src/lib/helpers/webBotAuthSignature.js
@@ -5,12 +5,6 @@ const authorityMessage = require('./authorityMessage')
 function webBotAuthSignature (method = 'GET', uri = '', signatureParams, privateJwk) {
   const message = authorityMessage(uri, signatureParams)
 
-  // const message = [
-  //   `"@method": ${method.toUpperCase()}`,
-  //   `"@target-uri": ${uri}`,
-  //   `"@signature-params": ${signatureParams}`
-  // ].join('\n')
-
   return crypto.sign(
     null,
     Buffer.from(message, 'utf8'),
