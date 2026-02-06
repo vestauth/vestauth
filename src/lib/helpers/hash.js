0794ecbd674f5bff443903a834b4b7256e1b60aa
Scott Motte <mot@mot.la>
2026-02-06 10:40:46 -0800

    clean up

--- src/lib/helpers/hash.js
+++
@@ -1,8 +0,0 @@
-const { sha256 } = require('@noble/hashes/sha2.js')
-const { utf8ToBytes } = require('@noble/hashes/utils.js')
-
-function hash (message = '') {
-  return sha256(utf8ToBytes(message))
-}
-
-module.exports = hash
