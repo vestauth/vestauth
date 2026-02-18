be14f4802f30a91d52c99af5997bf456522a950c
Scott Motte <mot@mot.la>
2026-02-17 16:44:26 -0800

    remove - unnecessary

--- src/lib/helpers/env.js
+++ src/lib/helpers/env.js
@@ -1,10 +1,6 @@
 const dotenvx = require('@dotenvx/dotenvx')
 
 function env (key) {
-  if (Object.prototype.hasOwnProperty.call(process.env, key)) {
-    return process.env[key]
-  }
-
   try {
     return dotenvx.get(key, { strict: true })
   } catch {
