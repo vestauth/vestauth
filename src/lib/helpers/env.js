80c03bbf4286df11e89f7b0c2060f4e7e843af52
Scott Motte <mot@mot.la>
2026-02-24 14:33:50 -0800

    respect preset env

--- src/lib/helpers/env.js
+++ src/lib/helpers/env.js
@@ -2,7 +2,7 @@ const dotenvx = require('@dotenvx/dotenvx')
 
 function env (key) {
   try {
-    return dotenvx.get(key, { strict: true })
+    return process.env[key] || dotenvx.get(key, { strict: true })
   } catch {
     return null
   }
